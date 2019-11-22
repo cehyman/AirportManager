e4ca525619cfac63446f2a1a47bf15c4704b9b95
peroroch <54253164+peroroch@users.noreply.github.com>
2019-11-22 15:40:38 -0500

    Update Alaska.java
    Added implementation for Alaska.java

--- Alaska.java
+++ Alaska.java
@@ -1,2 +1,51 @@
-public class Alaska {
+/**
+ * Alaska.java
+ *
+ * @author Duy Bui, Christopher Hyman, G20
+ * @version 11/22/2019
+ */
+
+public class Alaska implements Airline {
+    private final int maxSeats = 100;
+    private int currentSeats = 0;
+    private Passenger[] passengers = new Passenger[maxSeats];
+
+    public Alaska() {
+
+    }
+
+    @Override
+    public int getCurrentSeats() {
+        return currentSeats;
+    }
+
+    @Override
+    public int getMaxSeats() {
+        return maxSeats;
+    }
+
+    @Override
+    public String[] printPassengerList() {
+        String[] returnObject = new String[currentSeats];
+
+        for (int i = 0; i < currentSeats; i++) returnObject[i] = passengers[i].toString();
+
+        return returnObject;
+    }
+
+    @Override
+    public synchronized void addPassenger(Passenger passenger) {
+        passengers[currentSeats] = passenger;
+        currentSeats++;
+    }
+
+    @Override
+    public String getText() {
+        return "Alaska Airlines is proud to serve the strong and knowledgeable Boilermakers from Purdue University.\n" +
+                "We primarily fly westward, and often have stops in Alaska and California.\n" +
+                "We have first class amenities, even in coach class.\n" +
+                "We provide fun snacks such as pretzels and goldfish.\n" +
+                "We also have comfortable seats, and free WiFi.\n" +
+                "We hope you choose Alaska Airlines for your next itinerary!";
+    }
 }
