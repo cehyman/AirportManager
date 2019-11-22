9a0779e085d813228369c56aea1fda8554c8e13b
peroroch <54253164+peroroch@users.noreply.github.com>
2019-11-22 15:35:09 -0500

    Update Passenger.java
    Added implementation for Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,2 +1,23 @@
+/**
+ * Passenger.java
+ *
+ * @author Duy Bui, Christopher Hyman, G20
+ * @version 11/22/2019
+ */
+
 public class Passenger {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public Passenger(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName.substring(0, 1).toUpperCase() + ". " + lastName.toUpperCase() + ", " + age;
+    }
 }
