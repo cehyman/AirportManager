197537ac78f98d91eba26e88e34338aac66f9804
peroroch <54253164+peroroch@users.noreply.github.com>
2019-11-22 15:43:15 -0500

    Update Southwest.java
    Added implementation for Southwest.java

--- Southwest.java
+++ Southwest.java
@@ -1,2 +1,50 @@
-public class Southwest {
+/**
+ * Southwest.java
+ *
+ * @author Duy Bui, Christopher Hyman, G20
+ * @version 11/22/2019
+ */
+
+public class Southwest implements Airline {
+    private final int maxSeats = 100;
+    private int currentSeats = 0;
+    private Passenger[] passengers = new Passenger[maxSeats];
+
+    public Southwest() {
+
+    }
+
+    @Override
+    public int getCurrentSeats() {
+        return currentSeats;
+    }
+
+    @Override
+    public int getMaxSeats() {
+        return maxSeats;
+    }
+
+    @Override
+    public String[] printPassengerList() {
+        String[] returnObject = new String[currentSeats];
+
+        for (int i = 0; i < currentSeats; i++) returnObject[i] = passengers[i].toString();
+
+        return returnObject;
+    }
+
+    @Override
+    public synchronized void addPassenger(Passenger passenger) {
+        passengers[currentSeats] = passenger;
+        currentSeats++;
+    }
+
+    @Override
+    public String getText() {
+        return "Southwest Airlines is proud to offer flights to Purdue University.\n"+
+                "We are happy to offer free in flight wifi, as well as our amazing snacks.\n" +
+                "In additions, we offer flights for much cheaper than other airlines," +
+                " and offer two free checked bags.\n" +
+                "We hope you choose Southwest for your next flight.\n";
+    }
 }
