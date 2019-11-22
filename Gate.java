e53a4d2e18050f2d52d7bb3ecd4e9cd158fd741a
peroroch <54253164+peroroch@users.noreply.github.com>
2019-11-22 15:57:14 -0500

    Update Gate.java

--- Gate.java
+++ Gate.java
@@ -1,2 +1,26 @@
+/**
+ * Gate.java
+ *
+ * @author Duy Bui, G20
+ * @version 11/22/2019
+ */
+
 public class Gate {
+    private char letter;
+    private int number;
+    private Random random;
+
+    public Gate() {
+        this.random = new Random();
+        this.letter = (char) (random.nextInt(2) + 'A');
+        this.number = random.nextInt(17) + 1;
+    }
+
+    @Override
+    public String toString() {
+        String returnObject = "" + letter;
+        if (number < 10) {
+            return returnObject + "0" + number;
+        } else return returnObject + number;
+    }
 }
