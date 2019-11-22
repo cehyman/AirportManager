ea946ce28edf5bda658a8bee6caefb9bfd53fb11
peroroch <54253164+peroroch@users.noreply.github.com>
2019-11-22 14:43:51 -0500

    Update Airline.java
    added Airline.java

--- Airline.java
+++ Airline.java
@@ -1,4 +1,19 @@
 import java.io.Serializable;
 
+/**
+ * Airline.java
+ *
+ * An interface for airline classes
+ * 
+ * @author Duy Bui, Christopher Hyman, G20
+ * @version 11/22/2019
+ */
 public interface Airline extends Serializable {
+    int getCurrentSeats();
+    int getMaxSeats();
+    String getName();
+    String getPassenger();
+    String[] getPassengers();
+    void printPassengerList();
+    void addPassenger();
 }
